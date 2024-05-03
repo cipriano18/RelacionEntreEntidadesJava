3e06ed0be9cd179a8313495647813ce082d79f76
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-05-02 21:36:41 -0600

    attributes and their methods

--- Main/src/main/java/proyect/main/Department.java
+++ Main/src/main/java/proyect/main/Department.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyect.main;
 
-/**
- *
- * @author User
- */
 public class Department {
-    
+    private int ID;
+    private String name;
+
+    public Department(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ID =" + ID + " name=" + name ;
+    }
 }
